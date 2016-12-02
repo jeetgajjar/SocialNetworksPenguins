638e16f36c31b59723a77a77e5fbed8bdae2ac4d
Jeet G <j211668@gmail.com>
2016-12-02 01:07:10 -0500

    (de)compression edge, final script

--- main.py
+++ main.py
@@ -15,75 +15,102 @@ csv_writer = csv.writer(open('colorGraph.csv', 'wb'))
 
 max_key = 68719476736
 
+hash = {}
+
 
 def compress(array):
-
     red = array[0]
     green = array[1]
     blue = array[2]
-    int_rep_count = ((red / 4) * (64 ** 2)) + ((green / 4) * 64) + (blue / 4)
+    int_rep_color = ((red / 4) * (64 ** 2)) + ((green / 4) * 64) + (blue / 4)
 
-    return int_rep_count
+    return int_rep_color
 
 
-def decompress(int_rep_count):
+def decompress(int_rep_color):
 
-    blue = (int_rep_count % 64) * 4
-    green = ((int_rep_count / 64) % 64) * 4
-    red = (int_rep_count / (64 ** 2)) * 4
+    blue = (int_rep_color % 64) * 4
+    green = ((int_rep_color / 64) % 64) * 4
+    red = (int_rep_color / (64 ** 2)) * 4
 
     return red, green, blue
 
 
-with open('test.csv', 'rb') as csvfile:
+def edge_compression(source, target):
 
+    return long(source) * 262144 + long(target)
+
+
+def edge_decompression(int_rep_edge):
+
+    target = int(int_rep_edge % 262144)
+    source = int(int_rep_edge / 262144)
+
+    return source, target
+
+
+def look_left(list1, cur_x):
+    list1[cur_x -1]
+
+    return list1
+
+
+def look_up(list2, cur_y):
+    list1[cur_y -1]
+
+    return list2
+
+
+with open('test.csv', 'rb') as csvfile:
     x = 0
     y = 0
     list_pixel = []
+    list1 = []
+    list2 = []
+    counter = 0
 
     for line in csvfile:
         driver.get(line)
         driver.save_screenshot('line.png')
         im = Image.open('line.png').convert('RGB')
 
-        #This block prints all pixels (time should be ~15 seconds/img max)
+        # This block prints all pixels (time should be ~15 seconds/img max)
         pixel = im.load()
-        for x in xrange(0, 720, 10):
-            for y in xrange(0, 720, 10):
+        for y in xrange(0, 720, 10):
+            for x in xrange(0, 720, 10):
                 list_pixel = pixel[x, y]
+                cur_x = x/10
+                cur_y = y/10
 
-                #prints all 3-tuples, skipping 10 pixels
+                list1 += [compress(list_pixel)]
 
-                array = np.asarray(list_pixel)
-                compress(array)
-                #csv_writer.writerow(array)
+                if cur_x != 0:
+                   cur_edge_compression = edge_compression(list1[cur_x], list1[cur_x - 1])
+                   counter +=1
 
+                   try:
+                       hash[cur_edge_compression] += 1
+                   except KeyError:
+                       hash[cur_edge_compression] = 1
 
-
-                #TODO: Need to create adjacency list from 720x720 list
-
-
-        # black, white = im.getcolors()
-        # blue, green = im.getcolors()
-        # red = im.getcolors()
-
-        # for x in range(720):
-        #     for y in range(720):
-        #         color = im.getpixel((x, y))
-        #     print(color)
-        # for black, white, blue, green, red in im:
-        #
-        # print ('black: ', black[0])
-        # print ('white: ', white[0])
-        # print ('blue: ', blue[0])
-        # print ('green: ', green[0])
-        # print ('red: ', red[0])
-        # print
+                if cur_y != 0:
+                   cur_edge_compression = edge_compression(list1[cur_x], list2[cur_x])
+                   counter +=1
 
 
+                   try:
+                       hash[cur_edge_compression] += 1
+                   except KeyError:
+                       hash[cur_edge_compression] = 1
 
-        #Maybe create an if-statement to connect the colors together?
-        #Each pixel will have 2-3 neighbors
-        end_time = time.time()
-    final_time = end_time - start_time
-    print "Total time:", final_time
+            list2 = list(list1)
+            list1 = []
+    print counter
+
+
+    csv_writer.writerow(hash.items())
+
+
+end_time = time.time()
+final_time = end_time - start_time
+print "Total time:", final_time
